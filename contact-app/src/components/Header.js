e3da38567c5b25e7ca77c99898852fd6d724c1cd
hrishikeshh8 <hrishikeshh8@gmail.com>
2023-05-09 00:41:13 +0530

    hello

---
+++ contact-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Header = (props) => {
+    return(
+        <div>
+            
+                <h2>contact manager</h2>
+            
+        </div>
+    )
+}
+export default Header
